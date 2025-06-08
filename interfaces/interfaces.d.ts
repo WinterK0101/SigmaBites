4b1d04be4f26194fdd9aebac22ba991498fc9088
Michelle <michlaye@gmail.com>
2025-06-09 03:35:08 +0800

    Add UI for discover screen and collection of filters

--- interfaces/interfaces.d.ts
+++ interfaces/interfaces.d.ts
@@ -1,18 +1,45 @@
 export interface Eatery {
+    id: string;
     name: string;
-    address: string;
-    photoUrls?: string;
+    primaryCuisine: string;
     rating?: number;
     totalRatings?: number;
-    tags?: string[]; // Derived from 'types'
-    openNow?: boolean;
-    priceLevel?: number; // 0 to 4
+    priceLevel?: number;
+    distance: number;
+    isOpen?: boolean;
+    todayHours?: string;
+    photoReferences: string[];
     coords: {
         lat: number;
         lng: number;
     };
     placeId: string;
+}
+
+export interface EateryDetails extends Eatery {
+    address: string;
     phoneNumber?: string;
     website?: string;
-    openingHoursText?: string[];
+    allHours?: string[];
+    editorialSummary?: string;
+    reviews?: Review[];
+    googleMapsUri: string;
 }
+
+export interface Review {
+    authorName: string;
+    rating: number;
+    text: string;
+    relativeTime: string;
+    authorUrl?: string;
+    profilePhotoUrl?: string;
+}
+
+export interface EateryFilters {
+    priceLevels: number[];
+    minimumRating: number;
+    cuisineTypes: string[];
+    radius: number;
+    openNowToggle: boolean;
+}
+
