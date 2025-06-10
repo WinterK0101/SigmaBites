f97832743409fb9b61b25bbb10a2c836c98d9b8b
Michelle <michlaye@gmail.com>
2025-06-11 06:22:16 +0800

    removed extra screens

--- app/(tabs)/SignInPage.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function SignInPage() {
-    return (
-        <View>
-            <Text>Sign In Page</Text>
-        </View>
-    );
-}
--- app/(tabs)/StartPage.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function StartPage() {
-    return (
-        <View>
-            <Text>Start Page</Text>
-        </View>
-    );
-}
