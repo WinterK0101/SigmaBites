e2073f6f0f33e29c90e45123e74e58f339ef3e42
Michelle <michlaye@gmail.com>
2025-06-19 01:43:29 +0800

    conflicts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,88 +6,86 @@ import 'react-native-reanimated';
 import './globals.css';
 import { Session } from '@supabase/supabase-js'
 import { supabase } from '../SupabaseConfig';
-import SignInScreen from './SignInPage'
-import Profile from './(tabs)/Profile'
 import { SessionProvider } from '../context/SessionContext';
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+    // Catch any errors thrown by the Layout component.
+    ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+    // Ensure that reloading on `/modal` keeps a back button present.
+    initialRouteName: '(tabs)',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [fontsLoaded, error] = useFonts({
-    "Lexend-Bold": require('../assets/fonts/Lexend-Bold.ttf'),
-    "Lexend-Regular": require('../assets/fonts/Lexend-Regular.ttf'),
-    "Lexend-Variable": require('../assets/fonts/Lexend-Variable.ttf'),
-    "Baloo-Regular": require('../assets/fonts/Baloo-Regular.ttf'),
-  });
+    const [fontsLoaded, error] = useFonts({
+        "Lexend-Bold": require('../assets/fonts/Lexend-Bold.ttf'),
+        "Lexend-Regular": require('../assets/fonts/Lexend-Regular.ttf'),
+        "Lexend-Variable": require('../assets/fonts/Lexend-Variable.ttf'),
+        "Baloo-Regular": require('../assets/fonts/Baloo-Regular.ttf'),
+    });
 
-  const [session, setSession] = useState<Session | null>(null)
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+    const [session, setSession] = useState<Session | null>(null)
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session)
+        })
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session)
+        })
+    }, [])
 
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
 
-  useEffect(() => {
-    if (fontsLoaded) {
-      SplashScreen.hideAsync(); // If fonts have loaded, splash screen will be hidden
+    useEffect(() => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync(); // If fonts have loaded, splash screen will be hidden
+        }
+    }, [fontsLoaded]); // Checks if fonts are loaded
+
+    if (!fontsLoaded) {
+        return null;
     }
-  }, [fontsLoaded]); // Checks if fonts are loaded
 
-  if (!fontsLoaded) {
-    return null;
-  }
-
-  return (
-    <SessionProvider>
-      <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen
-              name="(modals)"
-              options={{ presentation: 'modal', headerShown: false }}
-          />
-        <Stack.Screen
-            name="(modals)/Swiping"
-            options={{
-              headerShown: false,
-            }}
-        />
-          <Stack.Screen
-              name="(modals)/Matched"
-              options={{
-                  headerShown: false,
-                  animation: "fade",
-              }}
-          />
-        <Stack.Screen
-            name="NoMatches"
-            options={{
-              headerShown: false,
-            }}
-        />
-      </Stack>
-    </SessionProvider>
-  );
+    return (
+        <SessionProvider>
+            <Stack>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen
+                    name="(modals)"
+                    options={{ presentation: 'modal', headerShown: false }}
+                />
+                <Stack.Screen
+                    name="(modals)/Swiping"
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+                <Stack.Screen
+                    name="(modals)/Matched"
+                    options={{
+                        headerShown: false,
+                        animation: "fade",
+                    }}
+                />
+                <Stack.Screen
+                    name="NoMatches"
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+            </Stack>
+        </SessionProvider>
+    );
 }
 
 function RootLayoutNav() {
