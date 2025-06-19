bb59aaf83651b02c5bf3ab4f580e593389a3c112
Michelle <michlaye@gmail.com>
2025-06-19 23:08:40 +0800

    group lobby and group invite screens

--- app/groupSwiping/GroupLobby.tsx
+++ app/groupSwiping/GroupLobby.tsx
@@ -1,10 +1,105 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import {View, Text, TouchableOpacity, Animated} from 'react-native'
+import React, {useState} from 'react'
+import {useLocalSearchParams, useRouter} from "expo-router";
+import {LinearGradient} from "expo-linear-gradient";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import {Collapsible} from "react-native-fast-collapsible";
 
 export default function GroupLobby() {
+    const { latitude, longitude, filters, useDummyData } = useLocalSearchParams();
+    const parsedFilters = filters ? JSON.parse(filters as string) : {};
+    const insets = useSafeAreaInsets();
+    const router = useRouter();
+
+    // For filters collapsible
+    const [isVisible, setVisibility] = useState(false);
+    const toggleVisibility = () => {
+        setVisibility((previous) => !previous);
+    };
+
     return (
-        <View>
-            <Text>GroupLobby</Text>
+        <View className="flex-1">
+            {/*Tab Header*/}
+            <LinearGradient
+                colors={['#d03939', '#fe724c']}
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingTop: insets.top,
+                    paddingHorizontal: 24,
+                    alignItems: 'center',
+                    height: 128,
+                }}
+            >
+                <Text className="font-baloo-regular text-white text-3xl mr-6">
+                    Group Lobby
+                </Text>
+                <TouchableOpacity onPress={() => router.back()} style={{ marginTop: -16 }}>
+                    <MaterialCommunityIcons name="door-open" size={36} color="white" />
+                </TouchableOpacity>
+            </LinearGradient>
+
+            <View className="px-6 flex-1">
+
+                {/*Filters*/}
+                <View className="bg-white p-5 mt-6 rounded-2xl border-2 border-grey">
+                    <TouchableOpacity className="flex-row justify-between" onPress={toggleVisibility}>
+                        <Text className="font-lexend-bold text-base text-primary">Filter Settings</Text>
+                        <Text style={{
+                            transform: isVisible ? [{ rotate: '90deg' }] : [{ rotate: '0deg' }],
+                        }}><MaterialCommunityIcons name="greater-than" size={20}/></Text>
+                    </TouchableOpacity>
+
+                    <Collapsible isVisible={isVisible}>
+                        <View className="mt-4">
+                            <View className="h-[2] bg-grey mb-2" />
+                            <Text className="text-primary text-xs mt-2 font-lexend-regular">
+                                <Text className="font-lexend-bold">Location: </Text>
+                                <Text>Enter location here</Text>
+                            </Text>
+                            <Text className="text-primary text-xs mt-2 font-lexend-regular">
+                                <Text className="font-lexend-bold">Search Radius: </Text>
+                                <Text>{parsedFilters.radius/1000} km</Text>
+                            </Text>
+                            <Text className="text-primary text-xs mt-2 font-lexend-regular">
+                                <Text className="font-lexend-bold">Price Levels: </Text>
+                                <Text>{parsedFilters.priceLevels.map((price: number) => '$'.repeat(price)).join(', ')}</Text>
+                            </Text>
+                            <Text className="text-primary text-xs mt-2 font-lexend-regular">
+                                <Text className="font-lexend-bold">Minimum Rating: </Text>
+                                <Text>{parsedFilters.minimumRating}</Text>
+                            </Text>
+                            <Text className="text-primary text-xs mt-2 font-lexend-regular">
+                                <Text className="font-lexend-bold">Prioritised Cuisines: </Text>
+                                <Text>
+                                    {parsedFilters.cuisineTypes.map((cuisine: string) => (cuisine)).join(', ')}
+                                </Text>
+                            </Text>
+                            <Text className="text-primary text-xs mt-2 font-lexend-regular">
+                                <Text className="font-lexend-bold">Only Open Eateries?: </Text>
+                                <Text>{parsedFilters.openNowToggle ? 'Yes' : 'No'}</Text>
+                            </Text>
+                            <TouchableOpacity className="mt-2"><Text className="font-baloo-regular text-accent">Edit Filters</Text></TouchableOpacity>
+                        </View>
+                    </Collapsible>
+                </View>
+
+                <Text className="text-accent text-2xl font-lexend-bold mt-6">Members (2/3)</Text>
+                <View className="flex-row justify-between items-center mt-2">
+                    <Text className="text-[#6C6C6C] font-lexend-regular text-base">2 joined, 1 pending</Text>
+                    <TouchableOpacity
+                        className="bg-white border-2 border-grey rounded-[30] w-[136] h-[36] justify-center items-center"
+                    >
+                        <Text className="text-accent text-xs font-baloo-regular">Invite More Friends</Text>
+                    </TouchableOpacity>
+                </View>
+
+
+
+            </View>
+
+
         </View>
     )
 }
--- app/groupSwiping/StartGroupSession.tsx
+++ app/groupSwiping/StartGroupSession.tsx
@@ -18,6 +18,7 @@ export default function StartGroupSession() {
     const [userFriends, setUserFriends] = useState<any[]>([]);
     const [invitedFriends, setInvitedFriends] = useState<any[]>([]);
     const [loading, setLoading] = useState(true);
+    const [searchQuery, setSearchQuery] = useState('');
 
 
     function inviteFriend(friend: any) {
@@ -50,6 +51,8 @@ export default function StartGroupSession() {
 
     return (
         <View style={{ flex: 1 }}>
+
+            {/*Tab Header*/}
             <LinearGradient
                 colors={['#d03939', '#fe724c']}
                 style={{
@@ -71,13 +74,16 @@ export default function StartGroupSession() {
 
             <View className="px-6 flex-1">
                 <Text className="font-lexend-bold text-accent text-2xl mt-8">Invite Friends</Text>
+
+                {/*Search Bar*/}
                 <View className="flex-row items-center bg-grey rounded-2xl px-5 py-4 mt-4">
                     <icons.search height={20} width={20} stroke="#6c6c6c" />
                     <TextInput
                         placeholder="Enter a username"
                         className="ml-3 flex-1 text-6c6c6c font-lexend-regular"
                         clearButtonMode="while-editing"
-                        onFocus={() => {}}
+                        onChangeText={(text) => setSearchQuery(text)}
+                        value={searchQuery}
                     />
                 </View>
 
@@ -121,38 +127,82 @@ export default function StartGroupSession() {
                 {/*List of Friends*/}
                 <Text className="font-lexend-bold text-base text-primary mt-6">Friends</Text>
 
-                <ScrollView className="mt-2 flex-grow">
+                <ScrollView className="mt-2 flex-grow" style={{ paddingBottom: 20 }}>
                     {userFriends.length === 0 ? (
                         <Text>No friends found.</Text>
                     ) : (
-                        userFriends.map((friend) => (
-                            <TouchableOpacity
-                                className="rounded-2xl w-full mb-2 h-20 border-2 px-6 flex-row items-center flex-start"
-                                key={friend.id}
-                                activeOpacity={0.7}
-                                onPress={()=>{
-                                    if (!invitedFriends.includes(friend)) inviteFriend(friend);
-                                    else uninviteFriend(friend);
-                                }}
-                                style={{
-                                    backgroundColor: invitedFriends.includes(friend) ? '#FFF2F2' : 'white',
-                                    borderColor: invitedFriends.includes(friend) ? '#FE724C' : '#d9d9d9',
-                                }}
-                            >
-                                <RemoteImage filePath={friend.avatar_url} bucket="avatars" style={{width: 50, height: 50, borderRadius: 100}} />
-                                <View className="flex-col ml-6">
-                                    <Text className="font-lexend-bold text-primary text-base">{friend.name}</Text>
-                                    <Text className="font-lexend-regular text-primary text-xs">@{friend.username}</Text>
-                                </View>
-                                {invitedFriends.includes(friend) ? (
-                                    <View className="ml-auto rounded-full bg-accent p-2">
-                                        <MaterialCommunityIcons name="check" size={24} color="white" />
+                        userFriends
+                            .filter(friend=>friend.username.toLowerCase().includes(searchQuery.toLowerCase()))
+                            .map((friend) => (
+                                <TouchableOpacity
+                                    className="rounded-2xl w-full mb-2 h-20 border-2 px-6 flex-row items-center flex-start"
+                                    key={friend.id}
+                                    activeOpacity={0.7}
+                                    onPress={()=>{
+                                        if (!invitedFriends.includes(friend)) inviteFriend(friend);
+                                        else uninviteFriend(friend);
+                                    }}
+                                    style={{
+                                        backgroundColor: invitedFriends.includes(friend) ? '#FFF2F2' : 'white',
+                                        borderColor: invitedFriends.includes(friend) ? '#FE724C' : '#d9d9d9',
+                                    }}
+                                >
+                                    <RemoteImage filePath={friend.avatar_url} bucket="avatars" style={{width: 50, height: 50, borderRadius: 100}} />
+                                    <View className="flex-col ml-6">
+                                        <Text className="font-lexend-bold text-primary text-base">{friend.name}</Text>
+                                        <Text className="font-lexend-regular text-primary text-xs">@{friend.username}</Text>
                                     </View>
-                                ) : null}
-                            </TouchableOpacity>
-                        ))
+                                    {invitedFriends.includes(friend) ? (
+                                        <View className="ml-auto rounded-full bg-accent p-2">
+                                            <MaterialCommunityIcons name="check" size={24} color="white" />
+                                        </View>
+                                    ) : null}
+                                </TouchableOpacity>
+                            ))
                     )}
                 </ScrollView>
+            </View>
+
+            {/*Go to Lobby*/}
+            <View
+                className="bg-white border-t border-grey"
+                style={{
+                    paddingBottom: insets.bottom + 16,
+                    paddingTop: 24,
+                    paddingHorizontal: 24,
+                }}
+            >
+                <TouchableOpacity
+                    className="bg-accent rounded-3xl justify-center items-center"
+                    style={{
+                        height: 60,
+                        shadowColor: '#FE724C',
+                        shadowOffset: { width: 0, height: 2 },
+                        shadowOpacity: 0.2,
+                        shadowRadius: 4,
+                        elevation: 4,
+                    }}
+                    activeOpacity={0.9}
+                    onPress={() => {{
+                        if (invitedFriends.length === 0) {
+                            alert("You have not invited any friends!");
+                            return;
+                        }
+                        router.push({
+                        pathname: '/groupSwiping/GroupLobby',
+                        params: {
+                            latitude,
+                            longitude,
+                            filters,
+                            useDummyData,
+                        }
+                        });
+                    }}}
+                >
+                    <Text className="font-baloo-regular text-white text-xl">
+                        Go to Lobby ({invitedFriends.length} invited)
+                    </Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
