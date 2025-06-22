7316eb03c29a49f40f62b9cd9b243a307fb255d4
Michelle <michlaye@gmail.com>
2025-06-23 06:35:39 +0800

    red dot on inbox button when user receives new message

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -61,14 +61,15 @@ export default function RootLayout() {
 
     return (
         <SessionProvider session={session} setSession={setSession}>
-            <Stack screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="index" />
-                <Stack.Screen name="(tabs)" />
-                <Stack.Screen name="(auth)" />
-                <Stack.Screen name="soloSwiping" />
-                <Stack.Screen name="groupSwiping" />
-                <Stack.Screen name="(modals)" options={{ presentation: 'modal' }} />
-            </Stack>
+                <Stack screenOptions={{ headerShown: false }}>
+                    <Stack.Screen name="index" />
+                    <Stack.Screen name="(tabs)" />
+                    <Stack.Screen name="(auth)" />
+                    <Stack.Screen name="soloSwiping" />
+                    <Stack.Screen name="groupSwiping" />
+                    <Stack.Screen name="(modals)" options={{ presentation: 'modal' }} />
+                </Stack>
+
         </SessionProvider>
     );
 }
